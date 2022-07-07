96f67d340c6a79e38a8dab366b0367fbdcfa23e9
anarkha18 <78490693+anarkha18@users.noreply.github.com>
2022-07-07 19:53:03 +0530

    hi

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -6,22 +6,28 @@ import { Employee } from '../models/employee.model';
   providedIn: 'root'
 })
 export class ApiService {
-  url = 'http://localhost:3000/users'
+  // url = 'http://localhost:3000/users'
+  url = 'http://127.0.0.1:8000/backend/users/';
   constructor(private http: HttpClient) { }
 
   createemployee(employee: Employee) {
-    return this.http.post('http://localhost:3000/users', employee);
+    // return this.http.post('http://localhost:3000/users', employee);
+    return this.http.post(this.url, employee);
   }
   getallemployees() {
-    return this.http.get('http://localhost:3000/users');
+    // return this.http.get('http://localhost:3000/users');
+    return this.http.get(this.url);
   }
   deleteemployee(id: number) {
-    return this.http.delete(`${this.url}/${id}`);
+    // return this.http.delete(`${this.url}/${id}`);
+    return this.http.delete(this.url + id + `/`);
   }
   getemployeeid(id: number) {
-    return this.http.get(`${this.url}/${id}`);
+    // return this.http.get(`${this.url}/${id}`);
+    return this.http.get(this.url + id + `/`);
   }
   updateemployee(id: number, data: any) {
-    return this.http.put(`${this.url}/${id}`, data);
+    // return this.http.put(`${this.url}/${id}`, data);
+    return this.http.put(this.url + id + `/`, data);
   }
 }
